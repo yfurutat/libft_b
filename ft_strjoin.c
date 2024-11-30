60b9dfed5c0ede0e9c9784c677a33147275611c4
Yuske <110963524+yfurutat@users.noreply.github.com>
2024-12-01 07:21:05 +0900

    Update ft_strjoin.c

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -20,24 +20,40 @@
  * @param s2 
  * @return char* 
  */
+// char	*ft_strjoin(char const *s1, char const *s2)
+// {
+// 	char	*nu_str;
+// 	size_t	i;
+// 	size_t	j;
+
+// 	if (!s1 || !s2)
+// 		return (NULL);
+// 	nu_str = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+// 	if (!nu_str)
+// 		return (NULL);
+// 	i = 0;
+// 	j = 0;
+// 	while (s1[j])
+// 		nu_str[i++] = s1[j++];
+// 	j = 0;
+// 	while (s2[j])
+// 		nu_str[i++] = s2[j++];
+// 	nu_str[i] = '\0';
+// 	return (nu_str);
+// }
+
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	char	*nu_str;
-	size_t	i;
-	size_t	j;
-
-	if (!s1 || !s2)
-		return (NULL);
-	nu_str = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
-	if (!nu_str)
-		return (NULL);
-	i = 0;
-	j = 0;
-	while (s1[j])
-		nu_str[i++] = s1[j++];
-	j = 0;
-	while (s2[j])
-		nu_str[i++] = s2[j++];
-	nu_str[i] = '\0';
-	return (nu_str);
+  char	*nu_str;
+  size_t  len_s1;
+  size_t  len_s2;
+  
+  len_s1 = ft_strlen(s1);
+  len_s2 = ft_strlen(s2);
+    nu_str = (char *)malloc(sizeof(char) * (len_s1 + len_s2 + 1));
+    if (!nu_str)
+        return (NULL);
+  ft_strlcpy(nu_str, s1, len_s1 + 1);
+  ft_strlcat(nu_str, s2, len_s1 + len_s2 + 1);
+  return (nu_str);
 }
