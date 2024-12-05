3fdb3ce3e5fa3c8e6fea605ef831b808c56e6bdd
Yuske <110963524+yfurutat@users.noreply.github.com>
2024-12-06 04:17:09 +0900

    Update ft_strdup.c

--- ft_strdup.c
+++ ft_strdup.c
@@ -36,3 +36,40 @@ char	*ft_strdup(const char *str)
 	dup[i] = '\0';
 	return (dup);
 }
+
+char	*ft_strdup(const char *str)
+{
+	char	*dup;
+	size_t	len;
+
+	len = ft_strlen(str) + 1;
+	dup = malloc(sizeof(char) * len);
+	if (dup != NULL)
+		ft_strlcpy(dup, str, len);
+	return (dup);
+}
+
+char	*ft_strndup(const char *str, size_t n)
+{
+	char	*dup;
+
+	dup = malloc(sizeof(char) * n);
+	if (dup != NULL)
+		ft_strlcpy(dup, str, n);
+	return (dup);
+}
+
+char	*ft_strndup(const char *str, size_t n)
+{
+	char	*dup;
+
+	dup = malloc(sizeof(char) * n);
+	if (dup != NULL)
+		dup = ft_strlcpy(dup, str, n);
+	return (dup);
+}
+
+char	*ft_strdup(const char *str)
+{
+	return (ft_strndup(str, ft_strlen(str)));
+}
