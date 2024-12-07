b926cc6956af35a568d1ef768ae13e43cd5b2a7e
Yuske <110963524+yfurutat@users.noreply.github.com>
2024-12-07 09:05:46 +0900

    Update ft_memset.c

--- ft_memset.c
+++ ft_memset.c
@@ -21,7 +21,7 @@
  * @param n_toset 
  * @return void* 
  */
-void	*ft_memset(void *mem, int chr, size_t num_byte_toset)
+void	*ft_memset(void *mem, int chr, size_t num_bytes_to_set_chr)
 {
 	unsigned char	*mem_caster;
 	unsigned char	chr_caster;
@@ -30,10 +30,34 @@ void	*ft_memset(void *mem, int chr, size_t num_byte_toset)
 	i = 0;
 	chr_caster = (unsigned char)chr;
 	mem_caster = (unsigned char *)mem;
-	while (i < num_byte_toset)
+	while (i < num_bytes_to_set_chr)
 	{
 		mem_caster[i] = chr_caster;
 		i += 1;
 	}
-	return (mem_caster);
+	return (mem);
+}
+
+//2
+void	*ft_memset(void *mem, int chr, size_t num_bytes_to_set_chr)
+{
+	size_t len;
+
+	len = iter_fill_str_with_chr_by_n((unsigned char*)mem, (unsigned char)chr, num_bytes_to_set_chr);
+	if (len != num_bytes_to_set_chr)
+		return (NULL);
+	return (mem);
+}
+
+size_t	iter_fill_str_with_chr_by_n(unsigned char *str, unsigned char c, size_t n)
+{
+	size_t i;
+
+	i = 0;
+	while (i < n)
+	{
+		str[i] = c;
+		i += 1;
+	}
+	return (i);
 }
