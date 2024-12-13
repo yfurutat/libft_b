984eac4a5000166c8a78a90e8211c9bba64e5f22
Yuske <110963524+yfurutat@users.noreply.github.com>
2024-12-14 07:06:11 +0900

    Update ft_strlcpy.c

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -23,21 +23,45 @@
  * @param len_cpy 
  * @return size_t 
  */
-size_t	ft_strlcpy(char *dest, const char *src, size_t len_cpy)
+void	iter_copy_src_to_dest(char *dest, const char *src, size_t end)
 {
 	size_t	i;
-	size_t	len_src;
 
 	i = 0;
-	len_src = ft_strlen(src);
-	if (len_cpy > 0)
+	while (end > i + 1 && src[i] != '\0')
 	{
-		while (len_cpy > i + 1 && src[i] != '\0')
-		{
-			dest[i] = src[i];
-			i += 1;
-		}
-		dest[i] = '\0';
+		dest[i] = src[i];
+		i += 1;
 	}
+	dst[i] = '\0';
+}
+
+size_t	ft_strlcpy(char *dest, const char *src, size_t len_cpy)
+{
+	size_t	len_src;
+
+	len_src = ft_strlen(src);
+	if (len_cpy == 0)
+		return (len_src);
+	iter_copy_src_to_dest(dest, src, total_len);
 	return (len_src);
 }
+
+// size_t	ft_strlcpy(char *dest, const char *src, size_t len_cpy)
+// {
+// 	size_t	i;
+// 	size_t	len_src;
+
+// 	i = 0;
+// 	len_src = ft_strlen(src);
+// 	if (len_cpy > 0)
+// 	{
+// 		while (len_cpy > i + 1 && src[i] != '\0')
+// 		{
+// 			dest[i] = src[i];
+// 			i += 1;
+// 		}
+// 		dest[i] = '\0';
+// 	}
+// 	return (len_src);
+// }
