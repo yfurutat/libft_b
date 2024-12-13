2fa5f565cebd5deea309fb7618081400c0350314
Yuske <110963524+yfurutat@users.noreply.github.com>
2024-12-14 02:34:27 +0900

    Update ft_memset.c

--- ft_memset.c
+++ ft_memset.c
@@ -57,3 +57,22 @@ void	*ft_memset(void *mem, int chr, size_t num_bytes_to_set_chr)
 // 	}
 // 	return ((char*)str);
 // }
+
+void	*ft_memset(void *mem, int chr, size_t num_bytes_to_set_chr)
+{
+	iter_fill_str_with_chr_by_n((unsigned char*)mem, (unsigned char)chr, num_bytes_to_set_chr);
+	return (mem);
+}
+
+size_t	iter_fill_str_with_chr_by_n(unsigned char *str, unsigned char c, size_t n)
+{
+	size_t i;
+
+	i = 0;
+	while (i < n)
+	{
+		str[i] = c;
+		i += 1;
+	}
+	return (i);
+}
