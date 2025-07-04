296c1d9d39d58b0d9fef53f15f12f2e84180801f
Yuske <110963524+yfurutat@users.noreply.github.com>
2025-07-05 00:36:05 +0900

    Update ft_memmove.c

--- ft_memmove.c
+++ ft_memmove.c
@@ -47,31 +47,31 @@
 // 	// if (dst == NULL && src == NULL)
 // 		// ft_memcpy(dst, src, n);
 
-void	*ft_memmove(void *dst, const void *src, size_t n)
-{
-	unsigned char		*dst_bin;
-	const unsigned char	*src_bin;
+void	*ft_memcpy(void *dest, const void *src, size_t n_cpy);
+static void	_iter_copy_src_to_dest(unsigned char *dest, const unsigned char *src, size_t end);
+static void	_iter_copy_src_to_dest_from_end(unsigned char *dest, const unsigned char *src, size_t end);
 
-	if (dst == src)
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	if (dest == src)
 		return (NULL);
-	dst_bin = (unsigned char *)dst;
-	src_bin = (const unsigned char *)src;
 	if (n == 0)
-		return (dst_bin);
+		return (dest);
 	else if (dst > src)
-	{
-		while (n > 0)
-		{
-			n -= 1;
-			dst_bin[n] = src_bin[n];
-		}
-	}
+		_iter_copy_src_to_dest_from_end((unsigned char *)dest, (const unsigned char *)src, n);
 	else
-		dst_bin = ft_memcpy(dst, src, n);
-	return (dst_bin);
+		return (ft_memcpy(dst, src, n));
+	return (dest);
 }
 
-static void	_iter_copy_src_to_dest(unsigned char *dest, const unsigned char *src, size_t end);
+static void	_iter_copy_src_to_dest_from_end(unsigned char *dest, const unsigned char *src, size_t end)
+{
+	while (end > 0)
+	{
+		end -= 1;
+		dst_bin[end] = src_bin[end];
+	}
+}
 
 void	*ft_memcpy(void *dest, const void *src, size_t n_cpy)
 {
